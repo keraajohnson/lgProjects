84a0936baa90b04f07e050a1d59b13148ca3025f
Kera <kera.andrea@gmail.com>
2018-11-01 09:41:30 -0400

    changes to nav and header

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,20 @@
 const hello = 'World';
 console.log(hello);
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+const navBar = $('.stickyNav');
+
+// Get the offset position of the navbar
+const sticky = navBar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navBar.addClass("sticky")
+    } else {
+        navBar.removeClass("sticky");
+    }
+} 
